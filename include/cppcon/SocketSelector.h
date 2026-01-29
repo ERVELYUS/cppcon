51c3619a08404d54b917068c06c55c99287a2b7e
Luzin Nikolai <luzinknick@gmail.com>
2026-01-30 00:09:06 +0300

    Added erase() to SocketSelector

--- include/cppcon/SocketSelector.h
+++ include/cppcon/SocketSelector.h
@@ -3,11 +3,14 @@
 #include <cppcon/BaseSocket.h>
 #include <poll.h>
 
+#include <algorithm>
+
 class SocketSelector {
   std::vector<struct pollfd> m_poll_fds{};
 
  public:
   void add(const BaseSocket& socket, const short mode);
+  void remove(const BaseSocket& socket);
   void wait(int timeout = -1);
   bool is_ready(const BaseSocket& socket);
 };
--- src/SocketSelector.cpp
+++ src/SocketSelector.cpp
@@ -9,6 +9,16 @@ void SocketSelector::add(const BaseSocket& socket, const short mode) {
   m_poll_fds.push_back({socket.get_fd(), mode, 0});
 }
 
+void SocketSelector::remove(const BaseSocket& socket) {
+  int fd_to_remove = socket.get_fd();
+
+  m_poll_fds.erase(std::remove_if(m_poll_fds.begin(), m_poll_fds.end(),
+                                  [fd_to_remove](const pollfd& pfd) {
+                                    return pfd.fd == fd_to_remove;
+                                  }),
+                   m_poll_fds.end());
+}
+
 void SocketSelector::wait(int timeout) {
   if (::poll(m_poll_fds.data(), m_poll_fds.size(), timeout) < 0) {
     throw std::runtime_error("poll() failed");
